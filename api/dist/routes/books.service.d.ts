a42aee8f0d804da84c672489f39051fc6eedf5ad
Qamar Stationwala <yopappy0@gmail.com>
2025-02-13 06:21:41 -0800

    Removing dist

--- api/dist/routes/books.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class BooksService {
-    books(): string;
-    searchBooks(bookName: any, author: any, minPages: any, maxPages: any): string;
-    getBook(id: any): string;
-    saveBook(bookName: any, author: any, pageCount: any): string;
-    updateBook(bookName: any, author: any, pageCount: any): string;
-    deleteBook(id: any): string;
-}
--- api/dist/src/entities/book.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class BookEntity {
-    id: number;
-    bookName: string;
-    author: string;
-    pageCount: number;
-    description: string;
-}
