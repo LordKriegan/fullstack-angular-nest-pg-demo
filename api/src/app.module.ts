c0bfd14a668a73c947098ef19219fe4558a27d81
Qamar Stationwala <yopappy0@gmail.com>
2025-02-26 14:42:41 -0800

    adding serve-static module and updating config files to put all dists into one folder

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dbconfig } from './config/dbconfig'
+import { dbconfig } from './config/dbconfig';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../client/browser')
+    }),
     TypeOrmModule.forRoot(dbconfig),
     RoutesModule
   ],
